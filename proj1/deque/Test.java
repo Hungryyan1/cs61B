0a1f838c9fd66acedefb3c3303c8bd1c0c061bcd
Hungryyan1 <hungryan666@gmail.com>
2024-07-16 17:48:02 +0800

    proj1 finished

--- proj1/deque/Test.java
+++ proj1/deque/Test.java
@@ -2,7 +2,7 @@ package deque;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println((-1) %8);
+        System.out.println();
     }
 
 }
---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,36 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class GuitarHero {
+    public static void main(String[] args) {
+        GuitarString[] strings = new GuitarString[37];
+        for (int i = 0; i < 37; i++) {
+            strings[i] = new GuitarString(440 * Math.pow(2, (i - 24) / 12.0 ) );
+        }
+
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+        while (true) {
+
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index != -1) {
+                    strings[index].pluck();
+                }
+            }
+
+            double sample = 0;
+            for (int i = 0; i < 37; i++) {
+                sample += strings[i].sample();
+            }
+
+            StdAudio.play(sample);
+
+            for (int i = 0; i < 37; i++) {
+                strings[i].tic();
+            }
+        }
+    }
+}
